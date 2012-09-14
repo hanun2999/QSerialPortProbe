a7dcfbeb12a7b5f43b8d3418c08080f82ee3bf4d
Jiří Pinkava <j-pi@seznam.cz>
2012-09-15 01:26:15 +0200

    finish interface

--- qserialportprobe.h
+++ qserialportprobe.h
@@ -6,11 +6,13 @@
 #include "../QSCPIDev/qserial.h"
 
 class QSerialPortProbe {
+    QString port;
+    QString
 public:
 
     class Device {
     public:
-        /** Supported protocols for detection. */
+        /** Supported protocols. */
         typedef enum {
             SCPI,
             MANSON_PS,
@@ -18,25 +20,55 @@ public:
             OTHER
         } Protocol;
 
+        /** Setup for device detection. */
+        struct Setup {
+            QSerial::BaudeRate_t baudeRate;
+            Protocol protocol;
+        };
+
+        /** Initialize new device detector for specified serial port. */
         Device(const char* port);
+        
+        /** Initialize new device detector for specified serial port. */
         Device(const QString &port);
 
-        /** Try open serial port and indentify device and its parametrs.
+        /** Return detected bauderate . */
+        QSerial::BaudeRate_t bauderate() const;
+
+        /** Try open serial port and indentify device and its parameters.
           @return True if success identify anny device, false otherwise.
           */
-        bool detect();
+        bool detect(const Setup setups[]);
 
+        /** Return device type/name/description. */
+        const QString &deviceName() const;
+
+        /** Return true if open() suceeds for serial port. */
         bool isOpenable() const;
-        int bauderate() const;
-        QString port() const;
-        QString protocol() const;
-        QString deviceName() const;
+
+        /** Return true if device supports pinpointing like beeping, led flashing
+          or other king of noise.
+          */
         bool isPinpointable() const;
+
+        /** Make device identify yourself, if supported.
+
+          Make beep or flash some led or so, depends on device type.
+          */
         void pinpoint() const;
+
+        /** Return serial port name. */
+        const QString &port() const;
+
+        /** Return detected protocol. */
+        const Protocol protocol() const;
+
+        /** Return string representation of protocol. */
+        const QString protocolString() const;
     };
 
     QSerialPortProbe();
-    Device list();
+    QVector<Device> list();
 };
 
 #endif // QSERIALPORTPROBE_H
